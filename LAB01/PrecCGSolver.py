9720d79a717a2ed279bf16f140f7d83dcd5260ba
Sriram katta <sriram.katta@fau.de>
2024-06-03 19:48:17 +0200

    precgsolver impl done

--- LAB01/PrecCGSolver.py
+++ LAB01/PrecCGSolver.py
@@ -49,13 +49,23 @@ def PrecCGSolver(A: np.array, b: np.array, delta=1.0e-6, verbose=0):
     countIter = 0
 
     L = IC.incompleteCholesky(A)
-    x = LLT.LLTSolver(L, b)
-    r = A @ x - b
+    x_j = LLT.LLTSolver(L, b)
+    r_j = A @ x_j - b
     # INCOMPLETE CODE STARTS
+    d_j = -LLT.LLTSolver(L, r_j)
+    while np.linalg.norm(r_j) > delta:
+        d_j_tilde = A @ d_j
+        rho_j = d_j.T @ d_j_tilde
+        t_j = r_j.T @ LLT.LLTSolver(L, r_j) / rho_j
+        x_j = x_j + t_j * d_j
+        r_old = r_j
+        r_j = r_old + t_j * d_j_tilde
+        beta_j = r_j.T @ LLT.LLTSolver(L, r_j) / (r_old.T @ LLT.LLTSolver(L, r_old))
+        d_j = -LLT.LLTSolver(L, r_j) + beta_j * d_j
 
     # INCOMPLETE CODE ENDS
-
+    x = np.copy(x_j)
     if verbose:
-        print('precCGSolver terminated after ', countIter, ' steps with norm of residual being ', np.linalg.norm(r))
+        print('precCGSolver terminated after ', countIter, ' steps with norm of residual being ', np.linalg.norm(r_j))
 
     return x
